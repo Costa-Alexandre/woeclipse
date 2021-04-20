b4380366161a568c222b703909d3f247397c51bf
Alex Costa <alexandre.costa@code.berlin>
2021-04-20 03:42:49 +0200

    linting website.py

--- woeclipse/website.py
+++ woeclipse/website.py
@@ -1,31 +1,27 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user,\
+                        login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+# from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-# Initialize database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
 db = SQLAlchemy(app)
 
-# Initialize Authentication
-# app.config['SECRET_KEY'] = '7ZUXn;R}#tX3(9v' # 🚧 TODO: The secret key should be in a environment variable
+login_manager = LoginManager()  # initialize flask_login
+login_manager.init_app(app)  # initialize flask_login with our app
+login_manager.login_view = 'index'  # redirect route when @login_required fails
 
-login_manager = LoginManager() # initialize flask_login
-login_manager.init_app(app) # initialize flask_login with our app
-login_manager.login_view = 'index' # redirect route when @login_required fails
 
 # Connect flask login with the user records in our database:
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Models
 
+# Models
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
@@ -37,15 +33,16 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(80))
     is_admin = db.Column(db.Boolean, default=False)
 
-# Routes
 
+# Routes
 @app.route('/')
 def index():
     if current_user.is_authenticated:
         # Show the home page only to logged in users:
         return render_template('index.html', username=current_user.username)
     else:
-        # If users aren't logged in they should be redirected to the signin page:
+        # If users aren't logged in they should be
+        # redirected to the signin page:
         return render_template('index.html')
 
 
@@ -63,7 +60,8 @@ def signup():
     if request.method == 'POST':
         # Run this when submitting the signup form:
         try:
-            # Assign form values to variables to make working with them easier later:
+            # Assign form values to variables to make working
+            # with them easier later:
             first_name = request.form.get('first_name')
             last_name = request.form.get('last_name')
             # birthday = datetime(request.form.get('birthday'))
@@ -73,31 +71,41 @@ def signup():
             password = request.form.get('password')
             password_confirmation = request.form.get('password_confirmation')
 
-
-            # Validations: Throw an error if anything is wrong with the input parameters:
+            # Validations: Throw an error if anything is wrong
+            # with the input parameters:
             if not email or not password or not username:
-                raise ValueError('Email, username or password parameter was missing.')
+                raise ValueError('Email, username or password\
+                                 parameter was missing.')
 
             if not password == password_confirmation:
                 raise ValueError('The two passwords are not matching.')
 
-            # Hash the password to be stored in the database in an encrypted format: 
+            # Hash the password to be stored in the database
+            # in an encrypted format:
             hashed_password = generate_password_hash(password, method='sha256')
 
             # Create a new use record in the database:
-            new_user = User(first_name=first_name, last_name=last_name, country=country, email=email, username=username, password=hashed_password)
+            new_user = User(first_name=first_name,
+                            last_name=last_name,
+                            country=country,
+                            email=email,
+                            username=username,
+                            password=hashed_password)
 
             db.session.add(new_user)
             db.session.commit()
-            
+
             # Login the user
             login_user(new_user)
 
             # Send the user to the home page after successful login:
             return redirect('/')
         except Exception as error_message:
-            # Rerender the signup page and display an error message if any of the code above failed:
-            return render_template('signup.html', error="User could not be created. " + str(error_message))
+            # Rerender the signup page and display an error
+            # message if any of the code above failed:
+            return render_template('signup.html',
+                                   error="User could not be created.\
+                                   " + str(error_message))
     else:
         # Run this when a user goes too the /signup route in the browser:
         return render_template('signup.html')
@@ -108,24 +116,31 @@ def signin():
     if request.method == 'POST':
         # Run this when submitting the signin form:
         try:
-            # Assign form values to variables to make working with them easier later:
+            # Assign form values to variables to make working
+            # with them easier later:
             username = request.form.get('username')
             password = request.form.get('password')
 
-            # Try to find a user record in the database with the given email address:
+            # Try to find a user record in the database with the
+            # given email address:
             user = User.query.filter_by(username=username).first()
 
-            # Make sure that the given password matches the encrypted password of the user from the database:
+            # Make sure that the given password matches the encrypted
+            # password of the user from the database:
             if user and check_password_hash(user.password, password):
-                # If the use exists and the encrypted password matches, login the user and send them to the home page:
+                # If the use exists and the encrypted password matches,
+                # login the user and send them to the home page:
                 login_user(user)
                 return redirect('/')
             else:
-                # If no user with the given email address was found or the password doesn't match throw an error:
-                raise ValueError('Could not login with given login parameters.')
-        except:
-        # Rerender the signin page with an error message if anything goes wrong in the code above:
-            return render_template('signin.html', error="Invalid username or password.")
+                # If no user with the given email address was found
+                # or the password doesn't match throw an error:
+                raise ValueError("Couldn't login with given login parameters.")
+        except Exception:
+            # Rerender the signin page with an error message
+            # if anything goes wrong in the code above:
+            return render_template('signin.html',
+                                   error="Invalid username or password.")
     else:
         # Run this when a user goes too the /signin route in the browser:
         return render_template('signin.html')
