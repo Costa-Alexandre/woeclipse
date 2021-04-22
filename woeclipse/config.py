92d549710fe9bed9c8063e04b8fa869a33be90cc
Alex Costa <alexandre.costa@code.berlin>
2021-04-23 01:04:59 +0200

    password exposed

--- woeclipse/config.py
+++ woeclipse/config.py
@@ -7,7 +7,7 @@ from os import environ
 # by adding it as a repository secret, like with our google cloud credentials.
 # Those passwords can then be added dynamically to app.yaml by the GitHub
 # Action step called "Prepare Deployment" on line 36 of main.yaml.
-SECRET_KEY = environ.get('DB_PASSWORD')
+SECRET_KEY = 'hBcszIsbavFpmOhB'
 SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://postgres:\
                         {SECRET_KEY}@35.198.159.181/woeclipse'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
