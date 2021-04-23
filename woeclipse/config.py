394a56020e135f3d6cb162ac8c16dfc39b92ca98
Alex Costa <alexandre.costa@code.berlin>
2021-04-23 02:55:01 +0200

    pytest is still complaining. This is messy

--- woeclipse/config.py
+++ woeclipse/config.py
@@ -9,15 +9,15 @@ try:
     env_vars = data['env_variables']
     for k,v in env_vars.items():
         environ[k] = v
+
+    SECRET_KEY = '123456'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:' + environ.get('DB_PASSWORD') + '@35.198.159.181/woeclipse'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 except:
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # You can then set a different password for your production database in GitHub,
 # by adding it as a repository secret, like with our google cloud credentials.
 # Those passwords can then be added dynamically to app.yaml by the GitHub
 # Action step called "Prepare Deployment" on line 36 of main.yaml.
-SECRET_KEY = '123456'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:' + environ.get('DB_PASSWORD') + '@35.198.159.181/woeclipse'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-print(SQLALCHEMY_DATABASE_URI)
