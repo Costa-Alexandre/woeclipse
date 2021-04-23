84f9b795266edaac85cda978596cba89869b66b3
Alex Costa <alexandre.costa@code.berlin>
2021-04-23 02:46:50 +0200

    loading app.yaml into config.py

--- woeclipse/config.py
+++ woeclipse/config.py
@@ -1,14 +1,20 @@
 # file to configure flask, loaded into our flask application
 # using the line: app.config.from_pyfile("config.py") in website.py
 from os import environ
+import yaml
 
-#
+with open('app.yaml', "r") as f:
+    data = yaml.load(f, Loader=yaml.BaseLoader)
+env_vars = data['env_variables']
+for k,v in env_vars.items():
+    environ[k] = v
+
 # You can then set a different password for your production database in GitHub,
 # by adding it as a repository secret, like with our google cloud credentials.
 # Those passwords can then be added dynamically to app.yaml by the GitHub
 # Action step called "Prepare Deployment" on line 36 of main.yaml.
 SECRET_KEY = '123456'
-db_pass = environ.get('DB_PASSWORD')
-MY_URI = 'postgresql+psycopg2://postgres:' + str(db_pass) + '@35.198.159.181/woeclipse'
-SQLALCHEMY_DATABASE_URI = MY_URI
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:' + environ.get('DB_PASSWORD') + '@35.198.159.181/woeclipse'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+print(SQLALCHEMY_DATABASE_URI)
