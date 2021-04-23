9080b6bd841db4e26251f75e8ea80053203d33fb
Alex Costa <alexandre.costa@code.berlin>
2021-04-23 03:09:13 +0200

    I believe it works. Need help figuring out how to pass the secret

--- woeclipse/config.py
+++ woeclipse/config.py
@@ -1,13 +1,11 @@
 # file to configure flask, loaded into our flask application
 # using the line: app.config.from_pyfile("config.py") in website.py
-from os import environ
 
 #
 # You can then set a different password for your production database in GitHub,
 # by adding it as a repository secret, like with our google cloud credentials.
 # Those passwords can then be added dynamically to app.yaml by the GitHub
 # Action step called "Prepare Deployment" on line 36 of main.yaml.
-SECRET_KEY = 'hBcszIsbavFpmOhB'
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://postgres:\
-                        {SECRET_KEY}@35.198.159.181/woeclipse'
+SECRET_KEY = '123456'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:hBcszIsbavFpmOhB@35.198.159.181/woeclipse'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
