5cd33cb7afa327a7dd45fff9dcee5a8886f7c52e
Alex Costa <alexandre.costa@code.berlin>
2021-04-23 05:20:27 +0200

    using env variables correctly

--- woeclipse/config.py
+++ woeclipse/config.py
@@ -1,8 +1,11 @@
 import os 
 
 
-SECRET_KEY = '123456'
-DATABASE_PASSWORD = os.getenv('DB_PASSWORD')
+SECRET_KEY = os.getenv('DB_SECRET')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_USER = os.getenv('DB_USER')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-DB_URL = 'postgresql+psycopg2://postgres:{pw}@35.198.159.181/woeclipse'.format(pw=DATABASE_PASSWORD)
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}/{db}'.format(user=DB_USER, pw=DB_PASSWORD, host=DB_HOST, db=DB_NAME)
 SQLALCHEMY_DATABASE_URI = DB_URL
