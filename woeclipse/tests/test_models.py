50dfa5c4892e7050eb8378522bf66e9ebd1b2560
Alex Costa <alexandre.costa@code.berlin>
2021-05-03 23:48:35 +0200

    created test files

--- woeclipse/tests/test_models.py
+++ woeclipse/tests/test_models.py
@@ -1,5 +1,10 @@
 from woeclipse.models import User
+import pytest
 
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(first_name='Pie', last_name='Testt', email='test@pytest.com', username='pytester', password='pytestpass', birthday='2000-01-01')
+    return user
 
 def test_new_user():
     """
@@ -7,9 +12,13 @@ def test_new_user():
     WHEN a new User is created
     THEN check the email, hashed_password, and is_admin are defined correctly
     """
-    user = User(email='test@pytest.com', username='pytester', password='pytestpass')
+    user = User(first_name='Pie', last_name='Testt', email='test@pytest.com', username='pytester', password='pytestpass', birthday='2000-01-01')
+    assert user.first_name == 'Pie'
+    assert user.last_name == 'Testt'
     assert user.email == 'test@pytest.com'
     assert user.username == 'pytester'
-    # assert user.password != 'pytestpass'
-    assert not user.is_admin
-    assert 1 == 1
+    assert user.password == 'pytestpass'
+    assert user.birthday == '2000-01-01'
+
+def test_new_user_fixture(new_user):
+    assert new_user.last_name == 'Testt'
