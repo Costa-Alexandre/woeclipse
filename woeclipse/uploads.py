ac672b5b88496767b3e7c8cbaed7c5f8fcbbbf53
Alex Costa <alexandre.costa@code.berlin>
2021-05-09 22:40:48 +0200

    trying without contentType arg

--- woeclipse/uploads.py
+++ woeclipse/uploads.py
@@ -13,4 +13,4 @@ def upload_blob(bucket_name, source_file, destination_blob_name):
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    blob.upload_from_file(source_file, content_type='image/jpeg')
+    blob.upload_from_file(source_file)
